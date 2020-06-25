591d0a26112fd6d9d64e1bb216a8a5ecfc7435d3
Papaia <43409674+ItsPapaia@users.noreply.github.com>
2020-06-25 11:51:31 +0300

    more global data

--- index.js
+++ index.js
@@ -5,48 +5,36 @@ const BASE = 'https://coronavirus-19-api.herokuapp.com';
 const pad = (n, c = 2) => String(n).padStart(c, '0');
 const formatDate = (d) => `${pad(d.getDate())}/${pad(d.getMonth() + 1)}/${pad(d.getFullYear())}`;
 const log = (...lines) => console.log(lines.flat().join('\n'));
+const formatData = (data) =>
+  [
+    `**סכ"ה מקרים:** ${localInfoIL.cases.toLocaleString()}`,
+    `**סכ"ה החלימו:** ${localInfoIL.recovered.toLocaleString()}`,
+    `**סכ"ה מתים:** ${localInfoIL.deaths.toLocaleString()}`,
+    `**סכ"ה בדיקות:** ${localInfoIL.totalTests.toLocaleString()}`,
+    `**חולים חדשים (היום):** ${localInfoIL.todayCases.toLocaleString()}`,
+  ].join('\n')(async () => {
+    const wh = new WebhookClient(process.env.WH_ID, process.env.WH_TOKEN);
 
-(async () => {
-  const wh = new WebhookClient(process.env.WH_ID, process.env.WH_TOKEN);
+    const [globalInfo, localInfoIL] = await Promise.all([
+      fetch(`${BASE}/countries/world`).then((res) => res.json()),
+      fetch(`${BASE}/countries/israel`).then((res) => res.json()),
+    ]);
 
-  const [globalInfo, localInfoIL] = await Promise.all([
-    fetch(`${BASE}/all`).then((res) => res.json()),
-    fetch(`${BASE}/countries/israel`).then((res) => res.json()),
-  ]);
+    log([
+      'Fetched data!',
+      `Global: ${JSON.stringify(globalInfo, null, 2)}`,
+      `Local (IL): ${JSON.stringify(localInfoIL, null, 2)}`,
+    ]);
 
-  log([
-    'Fetched data!',
-    `Global: ${JSON.stringify(globalInfo, null, 2)}`,
-    `Local (IL): ${JSON.stringify(localInfoIL, null, 2)}`,
-  ]);
+    const date = new Date();
+    const embed = new MessageEmbed()
+      .setTitle(`עדכון - ${formatDate(date)}`)
+      .setTimestamp(date.getTime())
+      .setFooter(`UNIX: ${date.getTime()}`)
+      .addField('ישראל', formatData(localInfoIL), true)
+      .addField('גלובאלי', formatData(globalInfo), true);
 
-  const date = new Date();
-  const embed = new MessageEmbed()
-    .setTitle(`עדכון - ${formatDate(date)}`)
-    .setTimestamp(date.getTime())
-    .setFooter(`UNIX: ${date.getTime()}`)
-    .addField(
-      'ישראל',
-      [
-        `**סכ"ה מקרים:** ${localInfoIL.cases.toLocaleString()}`,
-        `**סכ"ה החלימו:** ${localInfoIL.recovered.toLocaleString()}`,
-        `**סכ"ה מתים:** ${localInfoIL.deaths.toLocaleString()}`,
-        `**סכ"ה בדיקות:** ${localInfoIL.totalTests.toLocaleString()}`,
-        `**חולים חדשים (היום):** ${localInfoIL.todayCases.toLocaleString()}`,
-      ],
-      true
-    )
-    .addField(
-      'גלובאלי',
-      [
-        `**סכ"ה מקרים:** ${globalInfo.cases.toLocaleString()}`,
-        `**סכ"ה החלימו:** ${globalInfo.recovered.toLocaleString()}`,
-        `**סכ"ה מתים:** ${globalInfo.deaths.toLocaleString()}`,
-      ],
-      true
-    );
-
-  await wh.send(embed);
-  log(`Sent: ${JSON.stringify(embed.toJSON())}`);
-  process.exit(0);
-})();
+    await wh.send(embed);
+    log(`Sent: ${JSON.stringify(embed.toJSON())}`);
+    process.exit(0);
+  })();

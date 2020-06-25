3fe16393e8031f693e0f2e3cadb8af5ed8226d76
Papaia <43409674+ItsPapaia@users.noreply.github.com>
2020-06-25 13:05:38 +0300

    refractor

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+if (!process.env.WH_ID) require('dotenv').config();
 const fetch = require('node-fetch');
-const { MessageEmbed, WebhookClient } = require('discord.js');
 const BASE = 'https://coronavirus-19-api.herokuapp.com';
+const WH_URL = `https://discordapp.com/api/webhooks/${process.env.WH_ID}/${process.env.WH_TOKEN}?wait=true`;
 
 const pad = (n, c = 2) => String(n).padStart(c, '0');
 const formatDate = (d) =>
@@ -11,15 +12,17 @@ const toJSON = (thing) => JSON.stringify(thing, null, 2);
 const formatData = (data, tests = true) =>
   [
     `**סה"כ מקרים:** ${data.cases.toLocaleString()}`,
+    '',
+    `**חולים (כרגע):** ${data.active.toLocaleString()}`,
     `**סה"כ החלימו:** ${data.recovered.toLocaleString()}`,
     `**סה"כ מתים:** ${data.deaths.toLocaleString()}`,
+    '',
     `**חולים חדשים (היום):** ${data.todayCases.toLocaleString()}`,
+    `**מתים חדשים (היום):** ${data.todayDeaths.toLocaleString()}`,
     tests ? `**סה"כ בדיקות:** ${data.totalTests.toLocaleString()}` : '',
   ].join('\n');
 
 (async () => {
-  const wh = new WebhookClient(process.env.WH_ID, process.env.WH_TOKEN);
-
   const [globalInfo, localInfoIL] = await Promise.all([
     fetch(`${BASE}/countries/world`).then((res) => res.json()),
     fetch(`${BASE}/countries/israel`).then((res) => res.json()),
@@ -32,14 +35,30 @@ const formatData = (data, tests = true) =>
   ]);
 
   const date = new Date();
-  const embed = new MessageEmbed()
-    .setTitle(`עדכון - ${formatDate(date)}`)
-    .setTimestamp(date.getTime())
-    .setFooter(`UNIX: ${date.getTime()}`)
-    .addField('ישראל', formatData(localInfoIL), true)
-    .addField('גלובאלי', formatData(globalInfo, false), true);
+  const embed = {
+    title: `עדכון - ${formatDate(date)}`,
+    type: 'rich',
+    timestamp: date.toISOString(),
+    color: 0x3eaf7c,
+    footer: { text: `UNIX: ${date.getTime()}` },
+    fields: [
+      { name: 'ישראל', value: formatData(localInfoIL), inline: true },
+      { name: 'גלובאלי', value: formatData(globalInfo, false), inline: true },
+    ],
+  };
+  // const embed = new MessageEmbed()
+  //   .setColor(0x3eaf7c)
+  //   .setTitle(`עדכון - ${formatDate(date)}`)
+  //   .setTimestamp(date.getTime())
+  //   .setFooter(`UNIX: ${date.getTime()}`)
+  //   .addField('ישראל', formatData(localInfoIL), true)
+  //   .addField('גלובאלי', formatData(globalInfo, false), true);
 
-  await wh.send(embed);
-  log(`Sent: ${toJSON(embed.toJSON())}`);
+  await fetch(WH_URL, {
+    method: 'POST',
+    body: JSON.stringify({ embeds: [embed] }),
+    headers: { 'Content-Type': 'application/json' },
+  });
+  // log(`Sent: ${toJSON(embed.toJSON())}`);
   process.exit(0);
 })();

9ae1be34662cdf2a9a07b7c04ced610fe230ae2f
Papaia <43409674+ItsPapaia@users.noreply.github.com>
2020-06-26 14:13:31 +0300

    smol

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 if (!process.env.WH_ID) require('dotenv').config();
 const fetch = require('node-fetch');
+const { inspect } = require('util');
 const BASE = 'https://coronavirus-19-api.herokuapp.com';
 const WH_URL = `https://discordapp.com/api/webhooks/${process.env.WH_ID}/${process.env.WH_TOKEN}?wait=true`;
 
@@ -7,7 +8,7 @@ const pad = (n, c = 2) => String(n).padStart(c, '0');
 const formatDate = (d) =>
   `${pad(d.getDate())}/${pad(d.getMonth() + 1)}/${pad(d.getFullYear())}`;
 const log = (...lines) => console.log(lines.flat().join('\n'));
-const toJSON = (thing) => JSON.stringify(thing, null, 2);
+const prepare = (thing) => inspect(thing, { depth: Infinity });
 
 const formatData = (data, tests = true) =>
   [
@@ -30,8 +31,8 @@ const formatData = (data, tests = true) =>
 
   log([
     'Fetched data!',
-    `Global: ${toJSON(globalInfo)}`,
-    `Local (IL): ${toJSON(localInfoIL)}`,
+    `Global: ${prepare(globalInfo)}`,
+    `Local (IL): ${prepare(localInfoIL)}`,
   ]);
 
   const date = new Date();
@@ -46,19 +47,13 @@ const formatData = (data, tests = true) =>
       { name: 'גלובאלי', value: formatData(globalInfo, false), inline: true },
     ],
   };
-  // const embed = new MessageEmbed()
-  //   .setColor(0x3eaf7c)
-  //   .setTitle(`עדכון - ${formatDate(date)}`)
-  //   .setTimestamp(date.getTime())
-  //   .setFooter(`UNIX: ${date.getTime()}`)
-  //   .addField('ישראל', formatData(localInfoIL), true)
-  //   .addField('גלובאלי', formatData(globalInfo, false), true);
+  log(`Sending: ${prepare(embed.toJSON())}`);
 
   await fetch(WH_URL, {
     method: 'POST',
     body: JSON.stringify({ embeds: [embed] }),
     headers: { 'Content-Type': 'application/json' },
   });
-  // log(`Sent: ${toJSON(embed.toJSON())}`);
+
   process.exit(0);
 })();
